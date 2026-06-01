fcf5169c50b9b7d044ce938dfb4c33817c0e5b4e
Peter Tuoriniemi <fpetu005@edu.xamk.fi>
2026-06-01 21:58:21 +0300

    Added very basic movement to a gameobject to start testing with multiplayer

---
+++ Assets/Scripts/Game logic/PlayerController.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game_logic
+{
+    public class PlayerController : MonoBehaviour
+    {
+        //In editor
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float maxAcceleration;
+        [SerializeField] private float maxRotationSpeed;
+        
+        [Header("Debug")]
+        [SerializeField] private Vector3 acceleration;
+        [SerializeField] private Vector3 velocity;
+        [SerializeField] private Vector2 moveInput;
+        
+        Rigidbody rb;
+        float speed;
+        
+       
+
+        void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+
+        }
+
+        
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            float moveHorizontal = moveInput.x;
+            float moveVertical = moveInput.y;
+            // Speed adjust
+            Vector3 targetVelocity = transform.forward * (maxSpeed * moveVertical);
+            Vector3 deltaV =  targetVelocity - rb.linearVelocity;
+            acceleration = deltaV / Time.deltaTime;
+            acceleration = Vector3.ClampMagnitude(acceleration, maxAcceleration);
+            velocity = rb.linearVelocity;
+            rb.AddForce(acceleration, ForceMode.Acceleration);
+            
+            //Rotation
+            rb.MoveRotation(Quaternion.Euler(0, moveHorizontal * maxRotationSpeed * Time.deltaTime + rb.rotation.eulerAngles.y, 0));
+        }
+        
+        public void OnMove(InputValue value)
+        {
+            moveInput = value.Get<Vector2>();
+        }
+    }
+}
